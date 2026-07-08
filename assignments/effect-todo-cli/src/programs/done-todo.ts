0532b75bcedf451726b07dc0398bb9864ba947bc
joel <joel@chequer.io>
2026-07-09 02:16:51 +0900

    programs(application)

---
+++ assignments/effect-todo-cli/src/programs/done-todo.ts
@@ -0,0 +1,13 @@
+import { Clock, Effect } from "effect";
+import { todoIdFromString } from "../domain/todo-id";
+import { TodoRepository } from "../services/todo-repository";
+
+export const doneTodo = (idInput: string) =>
+  Effect.gen(function* () {
+    const id = yield* todoIdFromString(idInput);
+    const repository = yield* TodoRepository;
+    const completedAtMillis = yield* Clock.currentTimeMillis;
+    const completedTodo = yield* repository.markDone(id, completedAtMillis);
+
+    return completedTodo;
+  });
